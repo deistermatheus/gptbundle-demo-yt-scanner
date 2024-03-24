27c0b8ce7aa17916fd5328dce236548043464811
Matheus Deister <matheus.deister@vinta.com.br>
2024-03-24 07:08:30 -0300

    fix: hours in video timestamp

--- src/app/page-helpers.ts
+++ src/app/page-helpers.ts
@@ -4,7 +4,7 @@ function secondsToTime(seconds: number): string {
   const remainingSeconds = seconds % 60;
   const formattedString = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
 
-  if (formattedString.startsWith("00:")) {
+  if (hours === 0) {
     return formattedString.slice(3);
   }
 
